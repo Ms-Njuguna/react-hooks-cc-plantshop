ff47c2de671f959f45c6adc332601d63c56e1a68
Patricia <patricianjuguna084@gmail.com>
2025-07-12 21:14:56 +0300

    Add delete plant logic

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,22 @@
 import React, {useState} from "react";
 
-function PlantCard({name, image, price}) {
+function PlantCard({name, image, price, onDelete, id}) {
   const[inStock, setInStock] = useState(true);
 
   function handleToggle() {
     setInStock(prev => !prev);
   }
+
+  function handleDelete() {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE",
+    })
+    .then(() => {
+      onDelete(id)
+    }
+    )
+  }
+
 
   return (
     <li className="card" data-testid="plant-item">
@@ -17,6 +28,7 @@ function PlantCard({name, image, price}) {
       ) : (
         <button onClick={handleToggle}>Out of Stock</button>
       )}
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({plants}) {
+  const[deleted, setDeleted] = useState('');
+
+  function handleDelete() {
+    setDeleted(plants.splice(deleted,1));
+  }
+
   return (
     <ul className="cards">
       {plants.map((plant) => {
         return(
           <PlantCard 
             key={plant.id}
+            id={plant.id}
             name={plant.name}
             image={plant.image}
             price={plant.price}
+            onDelete={handleDelete}
           />
         );
       })}
